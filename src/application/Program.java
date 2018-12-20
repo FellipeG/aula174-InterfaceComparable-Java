1392fd6b0a4f90c33839c97ae617a3712eafa756
FellipeG <fellipegarciasrj@yahoo.com.br>
2018-12-20 18:09:01 -0200

    Added entitie employee and comparable to put list in alphabetical order by name

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,27 +7,32 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
+
+import model.entities.Employee;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the path of the file:");
 		String path = sc.nextLine();
-		List<String> list = new ArrayList<>();
+		List<Employee> list = new ArrayList<>();
 		try(BufferedReader br = new BufferedReader(new FileReader(path))) {
 			
 			String line = br.readLine();
 			while(line != null) {
-				list.add(line);
+				String[] vect = line.split(",");
+				list.add(new Employee(vect[0], Double.parseDouble(vect[1])));
 				line = br.readLine();
 			}
 			
 			Collections.sort(list);
-			for(String name : list) {
-				System.out.println(name);
+			for(Employee name : list) {
+				System.out.println(name.getName() + " - $" + String.format("%.2f", name.getSalary()));
 			}
 		}
 		catch (FileNotFoundException e) {
